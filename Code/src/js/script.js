bbaae14b9df912713294d3821b8644d39a4de6ed
NWChen <neilwchen@gmail.com>
2016-09-05 17:55:21 -0400

    CAD on rear end of shooter

---
+++ Code/src/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	// SPIN UP button pressed
+	$("#btn-spinup").click(function() {
+		console.log("SPIN UP");
+	});
+
+	// RAISE button pressed
+	$("#btn-raise").click(function() {
+		console.log("RAISE");
+	});
+
+	// LOWER button pressed
+	$("#btn-lower").click(function() {
+		console.log("LOWER");
+	});
+
+	// FIRE button pressed
+	$("#btn-fire").click(function() {
+		console.log("FIRE");
+	});
+
+	// HALT button pressed
+	$("#btn-halt").click(function() {
+		console.log("HALT");
+	});
+});
+
