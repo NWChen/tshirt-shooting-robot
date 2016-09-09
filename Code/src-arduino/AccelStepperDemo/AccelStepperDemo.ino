b62d0606e9b14b91caa8758ad6ddf0921160e706
NWChen <neilwchen@gmail.com>
2016-09-09 16:23:53 -0400

    Shooter controller as inline classes

---
+++ Code/src-arduino/AccelStepperDemo/AccelStepperDemo.ino
@@ -0,0 +1,62 @@
+#include <AccelStepper.h>
+/* This Rugged Motor Driver application demonstrates the use of the AccelStepper
+ * library for stepper motor control with ramp-up and ramp-down of speed.
+ * The AccelStepper library can be downloaded from:
+ *
+ *     http://www.open.com.au/mikem/arduino/AccelStepper
+ *
+ * Assumptions:
+ *   - bipolar stepper motor connected to the 2 phases
+ *   - 8V-30V supply connected to Vin
+ *   - Vin jumper is cut (J21) if Vin>15V (OK up to 24V for Ruggeduino)
+ *
+ * The behavior is as follows:
+ *
+ *   - The motor accelerates up to 300 steps per second for a total travel of 4000 steps
+ *   - The motor then decelerates and reverses direction for another travel of 4000 steps
+ *   - The process repeats
+ * 
+ * This software is licensed under the GNU General Public License (GPL) Version
+ * 3 or later. This license is described at
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Application Version 1.0 -- October 2011 Rugged Circuits LLC
+ * http://ruggedcircuits.com
+ */
+ 
+// Define an AccelStepper object controlling direction pins D12 and D13 (default
+// Rugged Motor Driver connections)
+AccelStepper stepper1(2,12,13);
+
+void setup()
+{ 
+  // Set power on pins D3 and D11, the ENABLE1 and ENABLE2 pins. The parameter
+  // is in the range 0 (no power) to 255 (maximum power).
+    analogWrite(3, 100);
+    analogWrite(11, 100);
+
+  // Set step speed in steps per second
+    stepper1.setMaxSpeed(300.0);
+
+  // Set acceleration/deceleration in steps per second per second
+    stepper1.setAcceleration(30.0);
+
+  // Begin motion
+    stepper1.moveTo(4000);
+
+  // Change from divide-by-64 prescale on Timer 2 to divide by 8 to get
+  // 8-times faster PWM frequency (976 Hz --> 7.8 kHz). This should prevent
+  // overcurrent conditions for steppers with high voltages and low inductance.
+  TCCR2B = _BV(CS21);
+}
+ 
+void loop()
+{
+  // Once the motor has reached its target position, decelerate and move to the
+  // same position in the other direction.
+    if (stepper1.distanceToGo() == 0) {
+      stepper1.moveTo(-stepper1.currentPosition());
+    }
+    stepper1.run();
+}
+// vim: syntax=c cindent expandtab ts=2 sw=2
---
+++ Code/src-arduino/shooter_controller/shooter_controller.ino
@@ -0,0 +1,113 @@
+#include <AccelStepper.h>
+#include <Servo.h>
+
+// *** OUTPUT ***
+class Output {
+  const boolean debug = false;
+  public:
+
+    void printDebug(String id, String str) {
+      if(debug) {
+        Serial.println(id + ": " + str);
+      }
+    }
+};
+
+// *** CYLINDER ***
+class Cylinder: public Output {
+  AccelStepper* stepper;
+  int switch_port;
+  
+  // Constructor
+  public: 
+  Cylinder(int _switch_port, int dir_pin1, int dir_pin2, float max_speed=200.0) {
+    switch_port = _switch_port;
+    pinMode(switch_port, INPUT);
+    stepper = new AccelStepper(2, dir_pin1, dir_pin2);
+    analogWrite(3, 100);
+    analogWrite(11, 100);
+    stepper -> setMaxSpeed(max_speed);
+    stepper -> setAcceleration(max_speed/10.0);
+  }
+  
+  // Index by the given number of chambers
+  void index(int chambers=1) {
+    stepper -> moveTo(chambers * 200); // gear ratio is 1:6, and 360/60 * 6 = 1, so 200*1 = 200 steps/chamber
+    stepper -> run();
+  }
+  
+  // Determine whether the cylinder has rotated the given number of chambers
+  boolean hasRotated() {
+    return stepper -> distanceToGo() == 0;
+  }
+  
+  boolean isAligned() {
+    return digitalRead(switch_port) == 1;
+  }
+  
+  // Stops the cylinder from rotating
+  void stop() {
+    stepper -> stop();
+  }
+  
+  void printDebug() {
+    Output::printDebug("CYLINDER", "foo");
+  }
+};
+
+// *** RACK ***
+class Rack: public Output {
+  Servo servo;
+  int pot_port;
+  
+  // Constructor
+  public: 
+  Rack(int _servo_port, int _pot_port) {
+    servo.attach(_servo_port);
+  }
+  
+  boolean retract() {
+    return 
+  }
+  
+  boolean extend() {
+  
+  }
+  
+  boolean isRetracted() {
+    return readPot() < 1;
+  }
+
+  boolean isExtended() {
+    return readPot() > 99;
+  }
+  
+  int readPot() {
+    return map(analogRead(pot_port), 23, 1000, 0, 100);
+  }
+  
+  void printDebug() {
+    Output::printDebug("RACK", "foo");
+  }
+};
+
+// *** WHEEL ***
+class Wheel {
+  // Constructor
+  public: Wheel() {
+  }
+};
+
+// *** INPUTS ***
+class Inputs {
+  // Constructor
+  public: Inputs() {
+  }
+};
+
+void setup() {
+}
+
+void loop() {
+}
+
